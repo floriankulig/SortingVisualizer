4028ead7011129e7dc85b97c0ffb1effe9ad7282
Florian Kulig <florian.kulig@web.de>
2020-02-26 22:12:21 +0100

    implemented merge sort

---
+++ GUI.py
@@ -0,0 +1,100 @@
+import turtle
+from sorts.merge_sort import merge_sort_animations
+import random as rd
+import time
+
+PRIMARY_COLOR = "#2a6fbe"  # blue
+SECONDARY_COLOR = "#ac1207"  # red
+LAST_MERGE_COLOR = "#7f2d97"  # purple
+FINAL_COLOR = "#08a110"  # green
+ARRAY_SIZE = 100
+# higher ANIMATION_SPEED to make animations faster (has to be int)
+# minimum is 1; default is 3
+ANIMATION_SPEED = 3
+
+
+class Bar(turtle.Turtle):
+    value = 0
+
+    def __repr__(self):
+        return f"{self.value}"
+
+
+class Gui:
+    def __init__(self):
+        self.screen = turtle.Screen()
+        self.screen.title("Sorting Algorithms Visualizer by © Florian Kulig")
+        self.screen.setup(width=1200, height=800)
+        self.screen.bgcolor("#f5f5f5")
+        self.screen.tracer(0, 0)
+        self.array = list()
+        self.bars = [Bar() for i in range(ARRAY_SIZE)]
+        self.bar_padding = 10
+        self.start_posX = -500  # where the bars start visually
+
+    def run(self):
+        turtle.update()
+        self.screen.mainloop()
+
+    def swap_bars(self, bar1_idx, bar2_idx):
+        x1, x2 = self.bars[bar1_idx].xcor(), self.bars[bar2_idx].xcor()
+        self.bars[bar1_idx].goto(x2, 0)
+        self.bars[bar2_idx].goto(x1, 0)
+
+    def visualize_sorted_array(self):
+        # visualize that array is sorted
+        for i, bar in enumerate(self.bars):
+            bar.color(FINAL_COLOR)
+        turtle.update()
+
+    def new_array(self):
+        i = 0
+        self.array = [rd.randint(10, 390) for j in range(ARRAY_SIZE)]
+        for i, bar in enumerate(self.bars):
+            value = self.array[i]
+            bar.value = value
+            bar.shape("square")
+            bar.color(PRIMARY_COLOR)
+            bar.speed(0)
+            bar.shapesize(
+                stretch_wid=bar.value/10, stretch_len=0.3, outline=0)
+            bar.penup()
+            bar.goto(self.start_posX + (i*self.bar_padding), 400-bar.value)
+
+        return self.bars
+
+    def merge_sort(self):
+        animations = merge_sort_animations(self.array)
+        for i in range(len(animations)):
+            color_change = True if not i % 3 == 2 else False
+            last_merge = i > len(animations) - (len(self.array)*3)
+            if color_change:
+                bar_one_idx, bar_two_idx = animations[i]
+                # Check if color has to be reset
+                if i % 3 == 0:
+                    color = SECONDARY_COLOR
+                else:
+                    color = PRIMARY_COLOR
+                # Style the bars with new color
+                if last_merge:
+                    self.bars[bar_one_idx].color(LAST_MERGE_COLOR)
+                else:
+                    self.bars[bar_one_idx].color(color)
+                self.bars[bar_two_idx].color(color)
+            else:
+                # ... or their new height
+                idx, new_height = animations[i]
+                self.bars[idx].value = new_height
+                self.bars[idx].shapesize(
+                    stretch_wid=new_height/10, stretch_len=0.3)
+                self.bars[idx].goto(
+                    self.bars[idx].xcor(), 400-self.bars[idx].value)
+
+                if last_merge:
+                    self.bars[idx].color(LAST_MERGE_COLOR)
+
+            # we don't update the screen every iteration to save performance
+            if i % ANIMATION_SPEED == 0:
+                turtle.update()
+
+        self.visualize_sorted_array()
---
+++ main.py
@@ -0,0 +1,13 @@
+from GUI import Gui
+
+
+def main():
+    gui = Gui()
+    gui.new_array()
+    gui.merge_sort()
+    # print(gui.bars)
+    gui.run()
+
+
+if __name__ == "__main__":
+    main()
