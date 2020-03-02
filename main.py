6c6ccf9e29640588965069bffebb688e8e7b6bcd
Florian Kulig <florian.kulig@web.de>
2020-03-02 20:59:48 +0100

    implemented quicksort

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from gui.GUI import Gui
 def main():
     gui = Gui()
     gui.new_array()
-    gui.merge_sort()
+    # gui.merge_sort()
+    gui.quicksort()
     gui.run()
 
 
--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -1,2 +1,23 @@
-def quicksort_animations():
-    pass
+def partition(array, low, high):
+    i = (low-1)
+    pivot = array[high]
+
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i+1
+            array[i], array[j] = array[j], array[i]
+
+    array[i+1], array[high] = array[high], array[i+1]
+    return (i+1)
+
+
+def quickSort(array, low, high):
+    if low < high:
+
+        # pi is partitioning index, array[p] is now
+        # at right place
+        pi = partition(array, low, high)
+        quickSort(array, low, pi-1)
+        quickSort(array, pi+1, high)
+
+    return array
