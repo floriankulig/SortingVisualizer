40c0556881ee8838338482a4f2d06188badedf3f
Florian Kulig <florian.kulig@web.de>
2020-03-02 21:04:56 +0100

    implemented quicksort

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -4,7 +4,7 @@ def partition(array, low, high):
 
     for j in range(low, high):
         if array[j] <= pivot:
-            i = i+1
+            i += 1
             array[i], array[j] = array[j], array[i]
 
     array[i+1], array[high] = array[high], array[i+1]
@@ -13,11 +13,9 @@ def partition(array, low, high):
 
 def quickSort(array, low, high):
     if low < high:
-
-        # pi is partitioning index, array[p] is now
-        # at right place
-        pi = partition(array, low, high)
-        quickSort(array, low, pi-1)
-        quickSort(array, pi+1, high)
+        # partition index to split array
+        part_idx = partition(array, low, high)
+        quickSort(array, low, part_idx-1)
+        quickSort(array, part_idx+1, high)
 
     return array
