75955331d2604b83c87eb2d89b101a7e408702ae
Florian Kulig <florian.kulig@web.de>
2020-03-04 20:46:02 +0100

    added animations list structure for GUI

--- sorts/quick_sort.py
+++ sorts/quick_sort.py
@@ -1,22 +1,32 @@
-def partition(array, low, high):
+def quick_sort_animations(array):
+    animations = list()
+    if len(array) <= 1:
+        return
+    quick_sort(array, 0, len(array)-1, animations)
+    return animations
+
+
+def make_partition(array, low, high, animations):
     i = (low-1)
     pivot = array[high]
 
     for j in range(low, high):
         if array[j] <= pivot:
             i += 1
+            animations.append([i, j])
+            animations.append([i, j])
             array[i], array[j] = array[j], array[i]
 
     array[i+1], array[high] = array[high], array[i+1]
     return (i+1)
 
 
-def quickSort(array, low, high):
+def quick_sort(array, low, high, animations):
     if low < high:
         # partition index to split array
-        part_idx = partition(array, low, high)
+        part_idx = make_partition(array, low, high, animations)
         # part_idx not included since it's already at right place
-        quickSort(array, low, part_idx-1)
-        quickSort(array, part_idx+1, high)
+        quick_sort(array, low, part_idx-1, animations)
+        quick_sort(array, part_idx+1, high, animations)
 
-    return array
+    return animations
