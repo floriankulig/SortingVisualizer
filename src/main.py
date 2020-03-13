909cf9e96aef298f1c44886eb8b769ad4c547a44
Florian Kulig <florian.kulig@web.de>
2020-03-13 21:59:32 +0100

    removed unnecessary code (all logic now in gui.py)

--- src/main.py
+++ src/main.py
@@ -4,12 +4,6 @@ import time
 
 def main():
     gui = Gui()
-    time.sleep(3)
-    gui.merge_sort()
-    time.sleep(2)
-    gui.new_array()
-    time.sleep(3)
-    gui.quicksort()
     gui.run()
 
 
